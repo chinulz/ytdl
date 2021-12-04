e7cafe3411cf993090b3d916d85c866738b9c60c
chinulz <86215698+chinulz@users.noreply.github.com>
2021-12-05 02:40:32 +0700

    Add files via upload

---
+++ ytmp3.py
@@ -0,0 +1,26 @@
+from pytube import YouTube
+import os 
+import time
+
+
+url = input("\nMasukan link yt : ")
+print("\nDownloading Audio...")
+time.sleep(2)
+print("proces")
+time.sleep(3)
+print("proces")
+time.sleep(4)
+print("proces")
+yt = YouTube(url)
+result = yt.streams.filter(only_audio=True).first()
+output_file = result.download("/sdcard/Download/ytdl")
+path, _ = os.path.splitext(output_file)
+
+os.rename(output_file, path + '.mp3')
+print("\nDone")
+time.sleep(2)
+print("Procesed !")
+time.sleep(3)
+print(yt.title, "\nSucces Downloading This Audio From YouTube")
+time.sleep(4)
+print("\n\nCreated By ChinuLz\n\n\n\n")
